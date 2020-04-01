342ee1c9dcb259c4624f6026cb9e9717ac4837f3
櫻井 貴央 <shibadog1121@gmail.com>
2020-04-01 16:27:18 +0900

    wip 04_block_simple_bot

--- 04_block/simple_bot.rb
+++ 04_block/simple_bot.rb
@@ -22,3 +22,28 @@
 # 4. クラスメソッドsettingは、引数を2つ取り、1つ目がキー名、2つ目が設定する値です
 #     1. settingメソッドに渡された値は、インスタンスメソッド `settings` から返されるオブジェクトに、メソッド名としてアクセスすることで取り出すことができます
 #     2. e.g. クラス内で `setting :name, 'bot'` と実行した場合は、respondメソッドに渡されるブロックのスコープ内で `settings.name` の戻り値は `bot` の文字列になります
+
+
+
+class SimpleBot
+  def ask(keyword)
+
+  end
+
+
+  class << self
+    def respond(keyword, &block)
+
+    end
+
+    def settings
+      @setting ||= Object.new
+    end
+
+    def setting(key, value)
+      settings.define_singleton_method(:key) do
+
+      end
+    end
+  end
+end
